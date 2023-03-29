ed4ff882a3bf56efef38aac649ace0c002db0813
khadim Hussain <khadimarman112@gmail.com>
2023-03-29 05:18:01 +0500

    push

--- src/Components/Crousal.js
+++ src/Components/Crousal.js
@@ -1,118 +1,64 @@
-import './carousel.css'  
-// import Slider from "react-slick";    
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
- 
-                     
+import './carousel.css'                     
 const Crousal = () => {
   
   return (
    <div classname="container">
-   <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+   <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-indicators my-0">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner mdqr1 ">
-    <div class="carousel-item active d-flex">
+  <div className="carousel-inner  ">
+    <div className="carousel-item active d-flex">
     <img src="/Images/im1.png" alt="im1" className="im1"  />
     <img src="/Images/im2.png" alt="im2" className="im2"  />
     <img src="/Images/im3.png" alt="im3" className="im3" />
-    <div class="carousel-caption d-none d-md-block slidem1">
-        <h5>innovation</h5>
+    <div className="carousel-caption d-none d-md-block slidem1">
+        <h5 className='hd'>innovation</h5>
       </div>
-      <div class="carousel-caption d-none d-md-block slidem2">
-        <h5>Data & Analysis</h5>
+      <div className="carousel-caption d-none d-md-block slidem2">
+        <h5 className='hd1'>Data & Analysis</h5>
         <p  className="pragph"> lorem is not Every time you expand it will generate a 30-words <br />dummy text.  </p>
       </div>
-      <div class="carousel-caption d-none d-md-block slidem3">
-        <h5>Risk Management</h5>
+      <div className="carousel-caption d-none d-md-block slidem3">
+        <h5 className='hd2'>Risk Management</h5>
       </div>
     </div>
-    <div class="carousel-item d-flex">
+    <div className="carousel-item slide d-flex">
     <img src="/Images/im1.png" alt="im1" className="im1"  />
     <img src="/Images/im2.png" alt="im2" className="im2"  />
     <img src="/Images/im3.png" alt="im3" className="im3" />
-    <div class="carousel-caption d-none d-md-block slidem1">
-        <h5>innovation</h5>
+    <div className="carousel-caption d-none d-md-block slidem1">
+        <h5  className='hd'>web development</h5>
       </div>
-      <div class="carousel-caption d-none d-md-block slidem2">
-        <h5>Data & Analysis</h5>
+      <div className="carousel-caption d-none d-md-block slidem2">
+        <h5 className='hd1'>Andriod Development</h5>
         <p className="pragph"> lorem is not Every time you expand it will generate a 30-words<br /> dummy text.  </p>
       </div>
-      <div class="carousel-caption d-none d-md-block slidem3">
-        <h5>Risk Management</h5>
+      <div className="carousel-caption d-none d-md-block slidem3">
+        <h5 className='hd2'>SEO</h5>
       </div>
     </div>
-    <div class="carousel-item d-flex">
+    <div className="carousel-item d-flex">
     <img src="/Images/im1.png" alt="im1" className="im1"  />
     <img src="/Images/im2.png" alt="im2" className="im2"  />
     <img src="/Images/im3.png" alt="im3" className="im3" />
-    <div class="carousel-caption d-none d-md-block slidem1">
-        <h5>innovation</h5>
+    <div className="carousel-caption d-none d-md-block slidem1">
+        <h5 className='hd'>Analytics</h5>
       </div>
-      <div class="carousel-caption d-none d-md-block slidem2">
-        <h5>Data & Analysis</h5>
+      <div className="carousel-caption d-none d-md-block slidem2">
+        <h5 className='hd1'>Cyber Security</h5>
         <p  className="pragph"> lorem is not Every time you expand it will generate a 30-words<br /> dummy text.  </p>
       </div>
-      <div class="carousel-caption d-none d-md-block slidem3">
-        <h5>Risk Management</h5>
+      <div className="carousel-caption d-none d-md-block slidem3">
+        <h5 className='hd2'>Data Science</h5>
       </div>
     </div>
   </div>
-  {/* <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button> */}
+
   <img className='bgimg' src='/Images/reactb.png'/>
 </div>
-
-
-   {/* <div className="fluid-container">
-   <div className='innovation'>
-     <h1>innovation</h1>
-     </div>
-     <div >
-      <h1 className="datadesc">Data & Analysis</h1>
-      <p className='prg'> lorem is not enerator. Every time you expand it, <br /> splitted into a few sentences.  </p>
-     </div>
-     <div className="risk">
-     <h1>Risk Management </h1>
-     </div>
- <Slider className="" {...settings}>
-      
-     
-  
-      <div style={{width:"100%"}}>
-      <img src="/Images/im1.png" alt="im1" style={{width:"500px", zIndex:"10",position:"relative",top:"50px",left:"30px"}} />
-      </div>
-  
-      <div  style={{width:"100%" }}>
-      <img src="/Images/im2.png" alt="im2" style={{width:"600px",position:"relative",right:"50px"}} />
-      </div>
-      <div style={{width:"100%"}}>
-      <img src="/Images/im3.png" alt="im3" style={{width:"500px",zIndex:"10px" ,position:"relative",top:"50px",right:"20px"}} />
-      </div>
-
-      <div style={{width:"100%"}}>
-      <img src="/Images/im1.png" alt="im1" style={{width:"500px", zIndex:"10",position:"relative",top:"50px",left:"30px"}} />
-      </div>
-  
-      <div  style={{width:"100%" }}>
-      <img src="/Images/im2.png" alt="im2" style={{width:"600px",position:"relative",right:"50px"}} />
-      </div>
-      <div style={{width:"100%"}}>
-      <img src="/Images/im3.png" alt="im3" style={{width:"500px",zIndex:"10px" ,position:"relative",top:"50px",right:"20px"}} />
-      </div>
-      
-    </Slider>
-     
-  </div>   */}
   </div>   
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,106 +3,106 @@ import './navbar.css'
 function Navbar() {
   return (
     <>
-   <nav class="navbar navbar-expand-lg navbar-light ">
-  <div class="container d-flex">
-    <a class="navbar-brand" href="#">
+   <nav className="navbar navbar-expand-lg navbar-light ">
+  <div className="container d-flex disp">
+    <a className="navbar-brand" href="#">
     <img width= "100px" src="/Images/logo.png" alt= "logo"/>
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-5 d-flex px-5  ">
-        <li class="nav-item mx-2 ">
-          <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-5 d-flex px-5 disp ">
+        <li className="nav-item mx-2 ">
+          <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
         </li>
        
-        <li class="nav-item dropdown mx-2">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown mx-2">
+          <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           Expertise
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Web Developmment</a></li>
+            <li><a className="dropdown-item" href="#">Andriod Development</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Search Engin Optimization</a></li>
           </ul>
         </li>
-        <li class="nav-item dropdown mx-2">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown mx-2">
+          <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           Industries
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">IT</a></li>
+            <li><a className="dropdown-item" href="#">Marketing</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Develoment</a></li>
           </ul>
         </li>
-        <li class="nav-item dropdown mx-2">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown mx-2">
+          <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           Capital Structure
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Architecture</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item dropdown mx-2">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown mx-2">
+          <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           People
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">single</a></li>
+            <li><a className="dropdown-item" href="#">Team</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item dropdown mx-2">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown mx-2">
+          <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           Careers
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Careers Garding</a></li>
+            <li><a className="dropdown-item" href="#">Decision Making</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item dropdown mx-2">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown mx-2">
+          <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           About Us
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">About</a></li>
+            <li><a className="dropdown-item" href="#">Our Company</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item dropdown mx-2">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown mx-2">
+          <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           Contact Us
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Contact</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item dropdown mx-3">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown mx-3">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           <img className='flage' src="https://images.unsplash.com/photo-1494419470281-65c2b001a42b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dWslMjBmbGFnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"  />
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">pak</a></li>
-            <li><a class="dropdown-item" href="#">Ind</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu bg-primary" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">pak</a></li>
+            <li><a className="dropdown-item" href="#">Ind</a></li>
+            <li><a className="dropdown-item" href="#">US</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
 
